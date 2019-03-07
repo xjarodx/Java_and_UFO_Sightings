2f325bec7505fecb67cd45e30059dfe2b38f7f09
The Hoff <11578521+xjarodx@users.noreply.github.com>
2019-03-06 23:15:24 -0500

    done. just need to finish the style.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,48 +1,46 @@
 // from data.js
-//var tableData = data;
+var tableData = data;
 
 // YOUR CODE HERE!
-var tbody = d3.select(tbody);
-
-data.forEach(function(UFOReport) {
-    console.log(UFOReport);
- });
-
- data.forEach(function(UFOReport) {
-    console.log(UFOReport);
-    var row = tbody.append("tr");
- });
-
- data.forEach(function(UFOReport) {
-    console.log(UFOReport);
-    var row = tbody.append("tr");
   
-    Object.entries(UFOReport).forEach(function([key, value]) {
-      console.log(key, value);
+  var tbody= d3.select("tbody");
+  var filtButton = d3.select("#filter-btn");
+  var inputDate = d3.select("#datetime");
+  var inputCity = d3.select("#city");
+  var inputState = d3.select("#state");
+  var inputCountry = d3.select("#country");
+  var inputShape = d3.select("#shape");
+
+
+  console.log(data);
+  
+  function loadTable(data) {
+    tbody.html("");
+    data.forEach((ufo)=> {
+     var row= tbody.append("tr");
+     Object.entries(ufo).forEach(([key, value]) => {
+         var cell= tbody.append("td");
+         cell.text(value);
+     });
     });
+  };
+
+  loadTable(tableData);
+
+  filtButton.on("click", function() {
+    
+    d3.event.preventDefault();
+
+    var dateValue = inputDate.property("value");
+    var cityValue = inputCity.property("value");
+    var stateValue = inputState.property("value");
+    var countryValue = inputCountry.property("value");
+    var shapeValue = inputShape.property("value");
+    console.log(cityValue);
+    console.log(dateValue);
+
+    var filtData = tableData.filter(fData => (fData.datetime == dateValue) || (fData.city == cityValue) || (fData.state == stateValue) || (fData.country == countryValue) || (fData.shape == shapeValue));
+    console.log(filtData);
+    loadTable(filtData);
   });
 
-  data.forEach(function(UFOReport) {
-    console.log(UFOReport);
-    var row = tbody.append("tr");
-  
-    Object.entries(UFOReport).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the weather report object
-      var cell = tbody.append("td");
-    });
-  });
-
-  data.forEach(function(UFOReport) {
-    console.log(UFOReport);
-    var row = tbody.append("tr");
-    Object.entries(UFOReport).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      // in the weather report object
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-  
